c83e95acc7f3bd9698c57c60a9945116c70acb06
baehaeli <haeleeeeleah@gmail.com>
2015-05-17 22:11:44 +0900

    student_management_system

---
+++ student_management_system.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct student_register
+{ 
+    int student_number;
+    int student_name;
+    int status;
+    int grade;
+};
+
+int grade
+{
+    printf("학점을 입력하세요.");
+    scanf("%d",&grade);
+    if(grade>=91 && grade<=100)
+        printf("A");
+    else if(grade>=81 && grade<=90)
+        printf("B");
+    else if(grade>=71 && grade<=80)
+        printf("C");
+    else if(grade>=61 && grade<=70)
+        printf("D");
+    else if(grade>=71 && grade<=80)
+        printf("F");
+    else
+        printf("학점을 입력하세요.");
+
+}
+
+int status
+{
+    printf("휴학이면 1 복학이면 2 를 입력하하세요");
+    scanf("%d",&status);
+}
+
+int search
+{
+    printf("검색을 하고 싶으면 search 를 입력하세요");
+    scanf("%d",&search);
+    if(search == 'search')
+    {printf("student_number, student_name, grade, status 중 하나를 입력하세요.");
+        scanf("%s", &want_search);
+        if (want_search == 'student_number')
+
+
+int remove
+{
+    printf("제거 할 학생의 학번을 입력하세요.");
+    scanf("%10d,
+            int main(void)
+{
+
